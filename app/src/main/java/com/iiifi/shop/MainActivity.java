bf708f1fc7ae3c865e2c1756dc22efe28210473b
庞振国 <zhenguo.pang@edencity.com>
2017-03-23 20:40:23 +0800

    init

---
+++ app/src/main/java/com/iiifi/shop/MainActivity.java
@@ -0,0 +1,27 @@
+package com.iiifi.shop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.iiifi.shop.handler.HandlerUtil;
+import com.iiifi.shop.widget.SplashScreen;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SplashScreen splashScreen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        splashScreen = new SplashScreen(this);
+        splashScreen.show(R.drawable.art_login_bg,
+                SplashScreen.FADE_OUT);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        HandlerUtil.getInstance(this).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                splashScreen.removeSplashScreen();
+            }
+        }, 3000);
+    }
+}
