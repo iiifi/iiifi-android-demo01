b48c0aea1915967586a8486fe7d680f2374cdb98
iiifi <ii@iiifi.com>
2017-03-24 14:45:17 +0800

    修改提示

--- app/src/main/java/com/iiifi/shop/activity/LoginActivity.java
+++ app/src/main/java/com/iiifi/shop/activity/LoginActivity.java
@@ -2,6 +2,7 @@ package com.iiifi.shop.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
