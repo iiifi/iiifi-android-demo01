1890bfe2303259ff8f1c6154510318d273d81a5c
iiifi <ii@iiifi.com>
2017-03-31 18:21:27 +0800

    修改BaseAdapter

--- app/src/main/java/com/iiifi/shop/activity/DynamicAddActivity.java
+++ app/src/main/java/com/iiifi/shop/activity/DynamicAddActivity.java
@@ -2,6 +2,7 @@ package com.iiifi.shop.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.iiifi.shop.view.DynamicAddView;
 
@@ -12,5 +13,14 @@ public class DynamicAddActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dynamic_add);
         new DynamicAddView(this);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
