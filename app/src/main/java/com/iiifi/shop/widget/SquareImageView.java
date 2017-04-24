9874058a67beae9d67f2737be9c07fbf42bee4ec
iiifi <ii@iiifi.com>
2017-04-24 17:23:32 +0800

    将控件增加绑定模式

---
+++ app/src/main/java/com/iiifi/shop/widget/SquareImageView.java
@@ -0,0 +1,29 @@
+package com.iiifi.shop.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by dmm on 2017/4/24.
+ */
+
+public class SquareImageView extends ImageView {
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    //传入参数widthMeasureSpec、heightMeasureSpec
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
