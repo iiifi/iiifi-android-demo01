cb9d49541d106cf49015b562677d8d65c20d2ce6
iiifi <ii@iiifi.com>
2017-04-25 17:10:00 +0800

    将控件增加绑定模式

--- app/src/main/java/com/iiifi/shop/modules/syscenter/fragment/FindFragment.java
+++ app/src/main/java/com/iiifi/shop/modules/syscenter/fragment/FindFragment.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.iiifi.shop.activity.R;
+import com.iiifi.shop.common.base.fragment.BaseFragment;
 import com.iiifi.shop.modules.dynamic.adapter.DynamicItemAdapter;
 import com.iiifi.shop.constant.IiifiConstant;
 import com.iiifi.shop.modules.dynamic.entity.Dynamic;
@@ -20,15 +21,15 @@ import com.iiifi.shop.modules.dynamic.entity.Dynamic;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.ButterKnife;
+
 /**
  * Created by donglinghao on 2016-01-28.
  */
-public class FindFragment extends Fragment {
+public class FindFragment extends BaseFragment {
 
     private View mRootView;
-    private List<Dynamic> dynamicList;
-    private DynamicItemAdapter adapter;
-    private SwipeRefreshLayout swipeRefresh;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -36,39 +37,31 @@ public class FindFragment extends Fragment {
             Log.e("666","FindFragment");
             mRootView = inflater.inflate(R.layout.find_fragment,container,false);
         }
-        Toolbar toolbar= (Toolbar) mRootView.findViewById(R.id.toolbar);
-        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
         ViewGroup parent = (ViewGroup) mRootView.getParent();
         if (parent != null){
             parent.removeView(mRootView);
         }
-        initDynamicList();
-        RecyclerView recyclerView= (RecyclerView) mRootView.findViewById(R.id.recycler_view);
-        GridLayoutManager layoutManager=new GridLayoutManager(this.getActivity(),1);
-        recyclerView.setLayoutManager(layoutManager);
-        adapter=new DynamicItemAdapter(R.layout.dynamic_item,dynamicList);
-        recyclerView.setAdapter(adapter);
+        build();
         return mRootView;
     }
 
-    public void initDynamicList(){
-        dynamicList=new ArrayList<Dynamic>();
-        Dynamic dynamic=new Dynamic();
-        dynamic.setId("1");
-        dynamic.setUserId("11");
-        dynamic.setNickName("你微笑时很美");
-        dynamic.setPhoto("https://pic1.zhimg.com/v2-1f7a10915ae4f50613f6ba5cada4230c_xl.jpg");
-        dynamic.setContent("     敬启者：紫衣侯竞死，吾实伤感，天下虽大，对手难寻，此人一死，吾更寂寞，吾至今方知求胜虽难，求败更不易。然七年之约，不可不赴，来年花朝，当赴中土，但愿东海之滨，有人能以三尺剑，赐我一败");
-        dynamic.setCreateTime("15小时前");
-        dynamic.setClientType("来自小米mix尊享版");
-        dynamic.setSendAddr("望京soho-T2-C1507");
-        dynamic.setCollectionNum(21024);
-        dynamic.setStarNum(20485);
-        dynamic.setCommentNum(269428);
-        dynamic.setIsCollection(IiifiConstant.TRUE);
-        dynamic.setIsStar(IiifiConstant.TRUE);
-        for(int i=0;i<10;i++){
-            dynamicList.add(dynamic);
-        }
+    @Override
+    public void initView() {
+        ButterKnife.bind(this,mRootView);
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    @Override
+    public void initClickEffect() {
+
+    }
+
+    @Override
+    public void initData() {
+
     }
 }
